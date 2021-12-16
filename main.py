c7e33e27f087626998e1ffe4cbdfd5e4340d4764
kilian longepe <kilian.longepe@gmail.com>
2021-12-16 17:39:16 +0100

    fix all bug

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template, jsonify, request , make_response
 from jinja2 import Template
+import scrap
 
 import os
 
@@ -17,7 +18,9 @@ def index():
 @app.route('/animelist', methods=['GET'])
 def read():
     
-    return render_template("animelist.html", index_link = indexLink)
+    jourDict = scrap.scrapAgenda()
+
+    return render_template("animelist.html", index_link = indexLink, jour_dict = jourDict)
 
 
 @app.errorhandler(404)
@@ -25,5 +28,5 @@ def not_found(error):
 	return make_response(jsonify({'error':'Not Found'}),404)
 
 if __name__ == "__main__":        
-    app.run(debug=True,host='127.0.0.1',port = port) #local
-	#app.run(host = "0.0.0.0", port = port, debug=True) #container  
+    # app.run(debug=True,host='127.0.0.1',port = port) #local
+	app.run(host = "0.0.0.0", port = port, debug=True) #container  
--- scrap.py
+++ scrap.py
@@ -6,51 +6,41 @@ URL = "https://www.adkami.com/agenda"
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-jourList = []
-titleList = []
-epsList = []
-horaireList = []
-agendaTitle = [[],[]]
-agendaEps = [[],[]]
-agendaHoraire = [[],[]]
-i = 0
-y = 0
-
-colone = soup.find_all('div', class_="colone")
-for jour in colone:
-    journee = jour.h3.text.strip()
-    jourList.append(journee)
-    if jour.h3.text == "Lundi" or jour.h3.text == "Mardi":
-        for a in jour.find_all('a',):
-            for info in a.select('div[class*="col-12 episode"]'):
+def scrapAgenda():
+    jourDict = { "Lundi":[], "Mardi":[], "Mercredi":[], "Jeudi":[], "Vendredi":[], "Samedi":[], "Dimanche":[]}
+    colone = soup.find_all('div', class_="colone")
+    for jour in colone:
+        titleList = []
+        epsList = []
+        horaireList = []
+        journee = jour.h3.text.strip()
+        if journee == "Lundi" or journee == "Mardi" or journee == "Mercredi":
+            for a in jour.find_all('a',):
+                for info in a.select('div[class*="col-12 episode"]'):
+                    horaire = info.span.text.strip()
+                    horaireList.append(horaire)
+                    for epis in info.find_all('div', class_="info"):
+                        episode = epis.find('p', class_="epis").text.strip()
+                        epsList.append(episode)
+                        titre = epis.find('p', class_="title").text.strip()
+                        titleList.append(titre)                    
+        else:
+            for info in jour.select('div[class*="col-12 episode"]'):
                 horaire = info.span.text.strip()
-                horaireList.append(horaire)
-                for epis in info.find_all('div', class_="info"):
-                    episode = epis.find('p', class_="epis").text.strip()
+                horaireList.append(horaire)            
+                for epis in info.find_all('div', class_= "info"):
+                    episode = epis.p.text.strip()
                     epsList.append(episode)
-                    titre = epis.find('p', class_="title").text.strip()
-                    titleList.append(titre)                    
-    else:
-        for info in jour.select('div[class*="col-12 episode"]'):
-            horaire = info.span.text.strip()
-            horaireList.append(horaire)            
-            for epis in info.find_all('div', class_= "info"):
-                episode = epis.p.text.strip()
-                epsList.append(episode)
-                for title in epis.find_all('a'):
-                    titre = title.p.text.strip()
-                    titleList.append(titre)
-    # horaireList.append("wall_wall")
-    # epsList.append("wall_wall")
-    # titleList.append("wall_wall")
-    # for y in range(len(titleList)):
-    agendaTitle[0][0] = titleList[0]
-    # for y in agendaEps:
-    #     agendaEps[i][y] = agendaEps[y]
-    # for y in agendaHoraire:
-    #     agendaHoraire[i][y] = agendaHoraire[y]
-    i += 1
+                    for title in epis.find_all('a'):
+                        titre = title.p.text.strip()
+                        titleList.append(titre)
 
-print(agendaTitle,agendaEps, agendaHoraire)
+        for i in range(len(titleList)):
+            animedict = {}
+            animedict["Title"]= titleList[i]
+            animedict["Epis"]=epsList[i]
+            animedict["Horaire"]=horaireList[i]
+            jourDict[journee].append(animedict) 
+    return jourDict
 
 
